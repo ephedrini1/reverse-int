3eab230f2bfeee32f659fa777d36517135b0694e
ephedrini1 <ephedrini@yandex.ru>
2021-12-27 22:20:36 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  if (n < 0) n = -n;
+  let result = '';
+   while (n) {
+       result += n % 10;
+       n = Math.floor(n / 10);
+   }
+   return result;
 }
